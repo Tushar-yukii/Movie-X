83f22da84443b71530994a1ac4609ec7eadd0f59
Tushar Tikkiwal <ttikkiwal@gmail.com>
2025-09-06 21:26:12 +0530

    MovieX

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -27,14 +27,14 @@ const Search = () => {
   );
 
   useEffect(() => {
-    const func = async () => {
+    const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
       } else {
         reset();
       }
-    };
-    func();
+    }, 500);
+    return () => clearTimeout(timeoutId);
   }, [searchQuery]);
 
   return (
@@ -90,6 +90,17 @@ const Search = () => {
               </Text>
             )}
           </>
+        }
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="mt-10 px-5">
+              <Text className="text-center text-gray-500">
+                {searchQuery.trim()
+                  ? "No movies found"
+                  : "Search for a movie..."}
+              </Text>
+            </View>
+          ) : null
         }
       />
     </View>
